b62c8bf48aec79e2c45e7c0aee4f6c243e30c0cd
Stephen Aguilar <stephenonline25@gmail.com>
2023-01-25 14:39:17 +0800

    Initial commit

---
+++ preFinalExam/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace preFi {
+
+    class Year {
+        public string nameOfStudent = "Aguilar, Stephen Joaquin G."; 
+        public int year = 2002;
+        private int yBorn
+        {
+            get { return year; }
+
+            set{ year = value; }
+        }
+
+        public int getAge(int currentYear) {
+            if (currentYear < yBorn) { return -1; } 
+            return currentYear - yBorn;
+        }
+
+        public string verify(int n) {
+            if (n == -1) {
+                return "Invalid Input";
+            }
+            return "Your name: " + nameOfStudent + "\n" + "Your year of birth: " + year + "\n" + "Your age: " + n + " years old";
+        }
+
+    }
+    class Program
+    {
+
+        public static void Main(string[] args) {
+            Year y = new Year();
+            int result = y.getAge(2022);
+            Console.WriteLine(y.verify(result));
+        }  
+    
+    }
+
+}
