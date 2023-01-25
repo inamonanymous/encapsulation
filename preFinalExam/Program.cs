050fd44a0b8dfd1eb199c35bf0b0522fd421124d
Stephen Aguilar <stephenonline25@gmail.com>
2023-01-25 15:16:17 +0800

    Initial commit

--- preFinalExam/Program.cs
+++ preFinalExam/Program.cs
@@ -1,38 +1,61 @@
 ﻿using System;
-namespace preFi {
+using System.Reflection;
 
-    class Year {
-        public string nameOfStudent = "Aguilar, Stephen Joaquin G."; 
-        public int year = 2002;
+namespace preFi
+{
+
+    class Year
+    {
+        public string nameOfStudent = "Aguilar, Stephen Joaquin G.";
+        public int year;
         private int yBorn
         {
             get { return year; }
 
-            set{ year = value; }
+            set { year = value; }
         }
 
-        public int getAge(int currentYear) {
-            if (currentYear < yBorn) { return -1; } 
+        public int getYear(int year) {
+
+            return this.year = year;
+
+        }
+
+        public int getAge(int currentYear)
+        {
+            if (currentYear < yBorn) { return -1; }
             return currentYear - yBorn;
         }
 
-        public string verify(int n) {
-            if (n == -1) {
+        public string verify(int n)
+        {
+            if (n == -1)
+            {
                 return "Invalid Input";
             }
-            return "Your name: " + nameOfStudent + "\n" + "Your year of birth: " + year + "\n" + "Your age: " + n + " years old";
+            return "Your name: " + nameOfStudent + "\n" + "Your year of birth: " + yBorn + "\n" + "Your age: " + n + " years old";
         }
 
     }
     class Program
     {
 
-        public static void Main(string[] args) {
+        public static void Main(string[] args)
+        {
+
             Year y = new Year();
-            int result = y.getAge(2022);
+
+            Console.WriteLine("Enter year of birth: ");
+            string s = Console.ReadLine();
+            int year1 = int.Parse(s);
+
+            int yearOfBorn = y.getYear(year1);
+            int result = y.getAge(2023);
+
             Console.WriteLine(y.verify(result));
-        }  
-    
+            
+        }
+
     }
 
 }
